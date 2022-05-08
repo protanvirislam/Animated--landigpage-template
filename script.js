131b98eaa556205769956839aaa73c840d57cc2d
codehunter350 <codehunterpro@gmail.com>
2022-05-08 21:03:42 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+AOS.init({
+    offset: 0, // offset (in px) from the original trigger point
+   delay: 500, // values from 0 to 3000, with step 50ms
+   duration: 500, // values from 0 to 3000, with step 50ms
+  });
